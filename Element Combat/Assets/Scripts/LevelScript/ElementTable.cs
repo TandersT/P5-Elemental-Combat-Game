1b21b697e9167cbf88a3c5f877c3b359088e96e8
Tobias F <tfrido14@student.aau.dk>
2016-11-01 12:01:54 +0100

    Updated ElementTable

--- Element Combat/Assets/Scripts/LevelScript/ElementTable.cs
+++ Element Combat/Assets/Scripts/LevelScript/ElementTable.cs
@@ -3,38 +3,37 @@ using System.Collections;
 
 public class ElementTable : MonoBehaviour {
     [SerializeField]
-	public static float elementWeight = 0.5f;
+	public static float elementWeight = 0.5f; // find a way to access this variable in unity inspector.
 	private static float strong = 1.5f;
 	private static float weak = 0.5f;
 	private static float same = 1.0f;
 
 	public static float lookUpElementMultiplier(string attackingElement, string defendingElement){
-		int col = 0, row = 0;
 		strong = 1.0f + elementWeight;
 		weak = 1.0f - elementWeight;
 		float[,] elementTable = {{same, strong,weak},{weak,same,strong},{strong,weak,same,}};
 
 		switch(attackingElement){
+			int row;
 			case "fire": row = 0;
-                Debug.Log("fire attack");
 			break;
 			case "earth": row = 1;
 			break;
 			case "water": row = 2;
 			break;
-			default : Debug.Log ("Row not specified.");
+			default : Debug.Log ("Row not valid:" + attackingElement);
 			break;
 		}
 
 		switch(defendingElement){
+			int col;
 			case "fire": col = 0;
 			break;
 			case "earth": col = 1;
 			break;
 			case "water": col = 2;
-                Debug.Log("water def");
-                break;
-			default : Debug.Log ("Column not specified.");
+            break;
+			default : Debug.Log ("Column not valid:" + defendingElement);
 			break;
 		}
 
