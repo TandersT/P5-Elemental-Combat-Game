d6dd082a04525f924009258dec80b16bb205d012
Tobias F <tfrido14@student.aau.dk>
2016-11-11 10:16:02 +0100

    Updated game logic with create level method and comments

--- Element Combat/Assets/Scripts/LevelScript/GameLogic.cs
+++ Element Combat/Assets/Scripts/LevelScript/GameLogic.cs
@@ -2,33 +2,56 @@
 using System.Collections;
 
 public class GameLogic : MonoBehaviour {
-	public int initialMinionAmount, playerRespawnTime, numberOfPlayers, initialMonsterAmount, minionSpawnAmount, monsterSpawnAmount, playersAlive, enemiesAlive;
-	public int currentLevel = 1;
+	public uint initialMinionAmount, playerRespawnTime, numberOfPlayers, initialMonsterAmount, minionSpawnAmount, monsterSpawnAmount;
+	private uint playersAlive, enemiesAlive;
+	public uint currentLevel = 1;
 	public Vector3 enemySpawnPosition1, numberOfPlayer, enemySpawnPosition2, enemySpawnPosition3;
 
-    public Vector3[] playerPositions; 
+    public Vector3[] playerPositions;
+
+    [Range(1,10)]
+	public float healthItemDropRateWeight;
+	[Range(1,10)]
+	public uint monsterAmountWeight;
+	[Range(1,10)]
+	public float healthItemHealAmountWeight;
+	public uint currentLevel; 
 
 	private void startGame(){
-
-	}
-
-	private void startLevel(int currentLevel){
-	
-	}
-
-	private void spawnEnemies(int minionSpawnAmount, int monsterSpawnAmount){
-	
-	}
-
-	private void spawnPlayers(int numberOfPlayers){
-	
-	}
-
-	private void endLevel(){
 		
 	}
 
-	private void checkGameState(int playersAlive, int enemiesAlive){
+	private void startLevel(uint currentLevel){
+		spawnEnemies(minionSpawnAmount, monsterSpawnAmount, currentLevel);
+		healthItemDropRate(currentLevel);
+		healthItemHealAmount(currentLevel);
+	}
+
+	private void spawnEnemies(uint minionSpawnAmount, uint monsterSpawnAmount, uint currentLevel){
+		minionSpawnAmount *= monsterAmountWeight * level;
+		monsterSpawnAmount *= monsterAmountWeight * level;
+		// choose where to spawn the enemies
+	}
+
+	private void spawnPlayers(uint numberOfPlayers){
+		// choose where to spawn players
+	}
+
+
+	public float healthItemDropRate(uint currentLevel){
+		float dropRate = 100.0f / (healthItemDropRateWeight * level);
+		return dropRate;
+		// make this work when enemies die
+	}
+
+		float healthItemHealAmount(uint currentLevel){
+			float healAmount = 100.0f / (healthItemHealAmountWeight * level);
+			return healAmount;
+			// make this work with enemies
+	}
+	
+
+	private void checkGameState(uint playersAlive, uint enemiesAlive){
 
 		if(playersAlive == 0){
 			endLevel();
@@ -40,7 +63,12 @@ public class GameLogic : MonoBehaviour {
 			startLevel(currentLevel++);
 		}
 	}
-
+	
+	private void endLevel(){
+		// destroy enemies
+		// destroy players
+	}
+	
 	// Use this for initialization
 	void Awake(){
 		startLevel (1);
