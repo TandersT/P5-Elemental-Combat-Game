3b7734b41669ade9fe29f6ac8c24eb5ed2f3088b
Tobias F <tfrido14@student.aau.dk>
2016-11-11 12:31:31 +0100

    health item script

---
+++ Element Combat/Assets/Scripts/LevelScript/HealthItemScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthItemScript : MonoBehaviour {
+	public float healAmount;
+	public void Update(){
+		Debug.Log("Healing: " + healAmount);
+	}
+    void OnCollisionEnter(Collision colTarget) {
+    	if(colTarget.gameObject.tag == "Player"){
+    		DestroyObject(gameObject);
+        	Debug.Log("HealthItem was destroyed");	
+    	}
+    }
+}
