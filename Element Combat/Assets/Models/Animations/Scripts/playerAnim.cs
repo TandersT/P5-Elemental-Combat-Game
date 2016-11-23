69317dda3ea541dc05b4488251cf2051ec64ae8a
Amanda Rodrigues <amandaciliberto11@gmail.com>
2016-11-23 14:47:06 +0100

    player walks when pressing 'w' and stops when not pressing 'w' anymore

---
+++ Element Combat/Assets/Models/Animations/Scripts/playerAnim.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerAnim : MonoBehaviour {
+
+	public Animator anim;
+
+	// Use this for initialization
+	void Start () 
+	{
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		// detect if we're pressing a button
+		if (Input.GetKeyDown ("w")) {
+			//-1 indicats where the animations are in the animator which is the base layer
+			anim.Play ("walk", -1, 0f);
+		} 
+
+//		else if (Input.GetMouseButtonDown (0)) {
+//			anim.Play ("attack", -1, 0f);
+//		} 
+//
+//		else 
+//		{
+//			anim.Play ("still", -1, 0f);
+//		}
+			
+}
+}
+
