6d971caf4d79c856761d0e3acc592d0bf0244ef3
Amanda Rodrigues <amandaciliberto11@gmail.com>
2016-12-01 13:42:05 +0100

    fix bullet stefan is stupid hahaahaaha

--- Element Combat Final Edit/Assets/Scripts/LevelScript/ProjectileScript.cs
+++ Element Combat Final Edit/Assets/Scripts/LevelScript/ProjectileScript.cs
@@ -6,11 +6,8 @@ public class ProjectileScript : MonoBehaviour {
     public string element2;
     public float baseDamage;
     void OnCollisionEnter(Collision colTarget) {
-        if (colTarget.gameObject.tag == "Enemy" ||  colTarget.gameObject.tag == "Player" || colTarget.gameObject.tag == "Bullet") {
+        if (colTarget.gameObject.tag == "Enemy" || colTarget.gameObject.tag == "Wall" || colTarget.gameObject.tag == "Player" || colTarget.gameObject.tag == "Bullet") {
             DestroyObject(gameObject);
-        }
-        if (colTarget.gameObject.tag == "Wall") {
-            Physics.IgnoreCollision(colTarget.gameObject.GetComponent<Collider>(), GetComponent<Collider>(), true);
         }
     }
 }
